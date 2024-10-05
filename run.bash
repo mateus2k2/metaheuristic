973b4cd4e30b9d0ea0ea77c2422477107dacd02c
Mateus Filipe <mateus.silva@petrosoftdesign.com>
2024-10-05 10:33:06 -0300

    add

--- run.bash
+++ run.bash
@@ -1,3 +1,4 @@
-cd src && python3 main.py run --input data/batch_VND.json --output data/outputs/results_VND.json --prints 1
-cd src && python3 main.py run --input data/batch_MFFD.json --output data/outputs/results_MFFD.json --prints 1
-cd src && python3 main.py run --input data/batch_localSearch5.json --output data/outputs/results_localSearch5.json --prints 1
+cd src
+python3 main.py run --input data/batch_VND.json --output data/outputs/results_VND.json --prints 1
+# python3 main.py run --input data/batch_MFFD.json --output data/outputs/results_MFFD.json --prints 1
+# python3 main.py run --input data/batch_localSearch5.json --output data/outputs/results_localSearch5.json --prints 1
